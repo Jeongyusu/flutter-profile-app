073d46eaedc422a89c90af45b6af5f737a8a40de
Jeongyusu <onlys2545@naver.com>
2023-10-04 17:59:16 +0900

    진행중

---
+++ lib/components/profile_tab.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ProfileTab extends StatefulWidget {
+  @override
+  _ProfileTabState createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildTabBar(),
+        Expanded(child: _buildTabBarView()),
+      ],
+    );
+  }
+
+  Widget _buildTabBar() {
+    return TabBar(
+      controller: _tabController,
+      tabs: [
+        Tab(
+            icon: Icon(
+          Icons.directions_car,
+          color: Colors.black,
+        )),
+        Tab(
+            icon: Icon(
+          Icons.directions_transit,
+          color: Colors.black,
+        )),
+      ],
+    );
+  }
+
+  Widget _buildTabBarView() {
+    return TabBarView(
+      controller: _tabController,
+      children: [
+        Container(color: Colors.green),
+        Container(color: Colors.red),
+      ],
+    );
+  }
+}
